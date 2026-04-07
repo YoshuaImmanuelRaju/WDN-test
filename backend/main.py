0b1bd82e55dcd34737f9fd27fb427dabce60ddb1
ANORA\yoshuaraju <yoshuaraju@anorasolutions.com>
2026-04-07 05:34:20 +0530

    Fix: wrong API fix

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "*"
         # "http://localhost:5173",
-        "https://wdn-test.vercel.app/"
+        # "https://wdn-test.vercel.app/"
     ],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
